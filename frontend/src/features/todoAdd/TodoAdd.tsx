1b5729378c0ab2db90417fad84d731f373e56f7e
Peter <hoffmannpeter82@gmail.com>
2023-07-11 19:52:25 +0200

    Use translation for setting placeholder of Input for adding Todo

--- frontend/src/features/todoAdd/TodoAdd.tsx
+++ frontend/src/features/todoAdd/TodoAdd.tsx
@@ -1,10 +1,13 @@
 import { Input } from "../../components/Input";
+import { useTranslation } from "../../hooks/useTranslation";
 import { ITodoAddProps } from "./ITodoAddProps";
 
 export const TodoAdd: React.FC<ITodoAddProps> = (props) => {
+  const { t } = useTranslation();
+
   return (
     <>
-      <Input placeholder="Enter todo" /> <button>+</button>
+      <Input placeholder={t.placeholderEnterTodo} /> <button>+</button>
     </>
   );
 };
