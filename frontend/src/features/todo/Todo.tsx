2c3fad63c969872419278069796d6bda1f255301
Yobuligo <hoffmannpeter82@gmail.com>
2023-07-12 23:19:09 +0200

    Align delete button for todos right

--- frontend/src/features/todo/Todo.tsx
+++ frontend/src/features/todo/Todo.tsx
@@ -1,13 +1,14 @@
 import { Card } from "../../components/card/Card";
 import { TodoDelete } from "../todoDelete/TodoDelete";
 import { ITodoProps } from "./ITodoProps";
+import styles from "./Todo.module.css";
 
 export const Todo: React.FC<ITodoProps> = (props) => {
   const onDelete = () => props.onDeleteTodo(props.todo);
   return (
     <Card>
       <div>{props.todo.text}</div>
-      <div>
+      <div className={styles.todoFooter}>
         <TodoDelete onDelete={onDelete} />
       </div>
     </Card>
